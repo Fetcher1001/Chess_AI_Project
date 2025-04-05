a69d3649756578637083d796b3d945813417c35d
Fetcher1001 <Fetcher_1001@outlook.com>
2025-04-05 19:52:06 +0200

    Add files via upload

--- Chess_AI_Project/Input_output_AI.py
+++ Chess_AI_Project/Input_output_AI.py
@@ -3,11 +3,15 @@ from square import Square
 from piece import *
 from neural_net import *
 from move import Move
-
+import numpy as np
 class InputOutput():
+
 
     def __init__(self):
         self.board_input = [[0 for i in range(COLS)] for n in range(ROWS)]
+        self.piece_list = ["pawn", "knight", "bishop", "rook", "queen", "king"]
+        self.find_piece_move = lambda x: np.argmax(x) # find most likely piece or move
+
 
 
     def read_input(self, board): #
@@ -22,4 +26,33 @@ class InputOutput():
 
 
     def ai_move(self, board):
-        ai_instance = chess_ai(board)
+        pass
+
+
+    def ai_input_output(self, board):
+        ai = chess_ai(board)
+        piece_move = ai.neural_net()
+        p = piece_move[0]
+        m = piece_move[1]
+
+        p_index = self.find_piece_move(p)
+        piece = self.piece_list[p_index]
+
+
+        move_index = self.find_piece_move(m)
+        move_row = move_index // 8
+        move_col = move_index % 8
+
+        return [piece, move_row, move_col]
+
+
+    def find_piece_position(self, piece_type, target_field, player_color):
+        possible_positions = []
+        for row in range(8):
+            for col in range(8):
+                if self.board[row][col] == piece_type and self.color[row][col] == player_color:
+                    legal_moves = self.board.calc_moves(row, col)
+                    if target_field in legal_moves:
+                        possible_positions.append((row, col))
+        return possible_positions
+
